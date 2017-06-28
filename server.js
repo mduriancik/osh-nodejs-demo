d9c565024067fa6a25ea011bff39add4b0b87a54
mduriancik <mduriancik>
2017-06-28 18:46:44 +0200

    display system informations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const os = require('os');
 
 // Constants
 const PORT = 8080;
@@ -9,7 +10,19 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  let env = JSON.stringify(process.env, null, ' ');
+  let version = require('./package.json').version;
+  res.send(
+    `<html><body>
+    <h3>System Infos (v: ${version})</h3>
+    <b>os: </b> ${os.type()}, ${os.release()}<br><br>
+    <b>host: </b>${os.hostname()}<br><br>
+    <b>network: </b> 
+      <pre>${JSON.stringify(os.networkInterfaces(), null, ' ')}</pre><br>
+    <b>cpus: </b> <pre>${JSON.stringify(os.cpus(), null, ' ')}</pre><br>
+    <b>env: </b>
+    <pre>${env}</pre>
+    </body></html>`);
 });
 
 app.listen(PORT, HOST);
